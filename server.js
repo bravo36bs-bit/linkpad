358caadd164dfca4e634d56cf14d2c98a18c50b6
bravo36bs-bit <bravo36bs@gmail.com>
2026-04-29 02:57:56 -0700

    fix static path

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const io = new Server(server, {
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 const SECRET_KEY = process.env.JWT_SECRET;
 
