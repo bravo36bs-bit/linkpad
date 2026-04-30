39f47c3614658cba64e9629145eaca64c6b6c3bb
bravo36bs-bit <bravo36bs@gmail.com>
2026-04-30 02:52:34 -0700

    switch to falcondb

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const db = mysql.createConnection({
     host    : process.env.DB_HOST,
     user    : process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: 'falcondb',
+    database: process.env.DB_NAME || 'falcondb',
     port    : process.env.DB_PORT || 3306,
     ssl     : { rejectUnauthorized: false }
 });
